d501b0bf6d74f86c2196af65234508613e56ce97
Joel Vestin <joel.vestin06@skola.stenungsund.se>
2024-11-26 12:06:55 +0100

    :)

---
+++ Intro programmering/Test 10/Test 10A.py
@@ -0,0 +1,21 @@
+def rövare(a):
+    return a + "o" + a
+
+def multiplicera(g, h):
+    return g * h
+
+def eko(b):
+    print(b + b + b)
+
+
+print(rövare("p"))
+
+print(rövare("s"))
+
+print(multiplicera(4, 5))
+
+print(multiplicera(3, 2))
+
+eko("hej")
+
+eko("då")
---
+++ Intro programmering/Test 10/Test 10B.py
@@ -0,0 +1,21 @@
+def maximum(a, b):
+    return a if a > b else b
+
+def addera(a, b):
+    return a + b
+
+def hej(a):
+    print("hej", a)
+    
+
+print(maximum(3, 7))
+
+print(maximum(11, 3))
+
+print(addera(3, 5))
+
+print(addera(10, 2))
+
+hej("Ada")
+
+hej("Babbage")
